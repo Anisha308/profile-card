2461e02efcaf5a09a792cc8f778c2026a4eb8aa5
Anisha <anishaanuu308@gmail.com>
2023-10-09 13:29:00 +0530

    updated all changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import Profile from './component/Profiles';
+import React from "react";
+import Profile from "./component/Profiles";
 
- const userData = [
-   { id: 1, name: "anisha", email: "anisha@gmail.com" },
-   { id: 2, name: "neslanes", email: "nesla@gmail.com" },
-   { id: 3, name: "arundhathy", email: "arundhathy@gmail.com" },
- ];
+const userData = [
+  { id: 1, name: "anisha", email: "anisha@gmail.com" },
+  { id: 2, name: "neslanes", email: "nesla@gmail.com" },
+  { id: 3, name: "arundhathy", email: "arundhathy@gmail.com" },
+];
 
 function App() {
   return (
-
-    <div>
-
+    <div className="list">
       {userData.map((user) => (
-        <Profile key={user.id} name={user.name} email={user.email}/>
+        <Profile key={user.id} name={user.name} email={user.email} />
       ))}
-
     </div>
-  )
+  );
 }
-
 
 export default App;
--- src/component/Profiles.js
+++ src/component/Profiles.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
+import './Profiles.css'
 
-function Profile({name,email}) {
-   
-    return (
-        <div>
-
-            <h2>{name}</h2>
-            <p>{email}</p>
-        </div>
-    )
+function Profile({ name, email }) {
+  return (
+    <div className="container">
+      <div className="profile-card">
+        <h2>{`${name}`}</h2>
+        <p>{`${email}`}</p>
+      </div>
+    </div>
+  );
 }
-
-
 
 export default Profile;
